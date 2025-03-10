f242c088761490fd95583167ed52e24444597aff
alexanderpina-guana <pinaa5533@gmail.com>
2025-03-10 08:38:56 -0700

    Update TargetDeath.cs

--- Assets/Scripts/TargetDeath.cs
+++ Assets/Scripts/TargetDeath.cs
@@ -1,15 +1,47 @@
 using UnityEngine;
+using TMPro;
 
 public class TargetDeath : SphereDeath
 {
+
+    public TextMeshProUGUI countText;
+    public int pointValue = 1;
+    
     private void Start()
     {
+
+            //for the score
+        if(countText != null)
+        {
+            SetCountText();
+        }
+        
         //register instance
         if(GameManager.instance != null)
         {
             GameManager.instance.RegisterDeath();
         }
     }
+
+       void OnTriggerEnter2D(Collider2D other)
+    {
+            GameManager.instance.AddScore(pointValue);
+            SetCountText();
+            Destroy(other.gameObject);
+        
+    }
+
+        //For the score
+    void SetCountText()
+    {
+        if (countText != null)
+        {
+            countText.text = "Score: " + GameManager.instance.GetScore().ToString();
+        }
+    }
+
+
+    
 
     public override void Die()
     {
