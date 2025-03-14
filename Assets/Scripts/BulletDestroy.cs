bb63122828e185a11104c17c4859a0e737d43ffc
alexanderpina-guana <pinaa5533@gmail.com>
2025-03-14 10:45:34 -0700

    Update BulletDestroy.cs

--- Assets/Scripts/BulletDestroy.cs
+++ Assets/Scripts/BulletDestroy.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 
 public class BulletDestroy : MonoBehaviour
 {
+
+    public AudioClip hurtSound;
+    public float soundVolume = 1.0f;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if(audioSource == null)
+        { 
+            audioSource = gameObject.AddComponent<AudioSource>();
+
+        }
+       
+
+        audioSource.spatialBlend = 1.0f;
+        audioSource.minDistance = 1f;
+        audioSource.maxDistance = 50f;
+        audioSource.rolloffMode = AudioRolloffMode.Linear;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+     if(hurtSound != null)
+        {
+            audioSource.transform.position = transform.position;
+
+            audioSource.PlayOneShot(hurtSound);
+        }
+        
         // Destroys other object
         Destroy(collision.gameObject);
         // Destroys itself
