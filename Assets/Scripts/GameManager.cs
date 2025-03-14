d7ec9cf8ad0d6197843e536be47ef290b1ee55af
alexanderpina-guana <pinaa5533@gmail.com>
2025-03-14 10:56:04 -0700

    Added all sound based code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,15 @@ public class GameManager : MonoBehaviour
     private int targetDeathCount = 0;
     public int TargetDeathCount => targetDeathCount;
 
+    //For Audio
+    public AudioMixer audioMixer;
+
+    //For Volume Sliders
+    public Slider mainVolumeSlider;
+    public Slider musicVolumeSlider;
+    public Slider sfxVolumeSlider;
+   
+
     private void Awake()
     {
         if(instance == null)
@@ -27,6 +36,71 @@ public class GameManager : MonoBehaviour
       private void Start()
     {
         score = 0;
+
+        //Setting volumes
+        LoadVolumeSettings();
+
+        //Add Listeners
+        mainVolumeSlider.onValueChanged.AddListener(UpdateMainVolume);
+        musicVolumeSlider.onValueChanged.AddListener(UpdateMusicVolume);
+        sfxVolumeSlider.onValueChanged.AddListener(UpdateSFXVolume);
+    }
+
+
+    private void LoadVolumeSettings()
+    {
+        float mainVolume = PlayerPrefs.GetFloat("MainVolume", .5f);
+        float musicVolume = PlayerPrefs.GetFloat("MusicVolume", .5f);
+        float sfxVolume = PlayerPrefs.GetFloat("SFXVolume", .5f);
+
+        mainVolumeSlider.value = Mathf.Pow(10, mainVolume / 20);
+        musicVolumeSlider.value = Mathf.Pow(10, musicVolume / 20);
+        sfxVolumeSlider.value = Mathf.Pow(10, sfxVolume / 20);
+    }
+
+    private void UpdateMainVolume(float value)
+    {
+        audioMixer.SetFloat("MainVolume", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("MainVolume", value);
+    }
+
+    private void UpdateMusicVolume(float value)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", value);
+    }
+
+        private void UpdateSFXVolume(float value)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", value);
+    }
+
+    private void SetInitialSliderValues()
+    {
+        float mainVolume, musicVolume, sfxVolume;
+        audioMixer.GetFloat("MainVolume", out mainVolume);
+        audioMixer.GetFloat("MusicVolume", out musicVolume);
+        audioMixer.GetFloat("SFXVolume", out sfxVolume);
+
+        mainVolumeSlider.value = Mathf.Pow(10, mainVolume / 20);
+        musicVolumeSlider.value = Mathf.Pow(10, musicVolume / 20);
+        sfxVolumeSlider.value = Mathf.Pow(10, sfxVolume / 20);
+    }
+
+    private void UpdateMainVolumeWithDB(float value)
+    {
+        audioMixer.SetFloat("MainVolume",Mathf.Log10(value)* 20);
+    }
+
+    private void UpdateMusicVolumeWithDB(float value)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(value) * 20);
+    }
+
+    private void UpdateSFXVolumeWithDB(float value)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(value) * value);
     }
 
 
