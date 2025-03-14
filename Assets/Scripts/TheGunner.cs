9c8f2eb93beab807ec9fbaf1e15cf7197687df82
alexanderpina-guana <pinaa5533@gmail.com>
2025-03-14 11:02:58 -0700

    Update TheGunner.cs

--- Assets/Scripts/TheGunner.cs
+++ Assets/Scripts/TheGunner.cs
@@ -6,6 +6,25 @@ public class TheGunner : MonoBehaviour
     //For shooting
     public GameObject green_body_circle;
 
+    //For Audio
+    public AudioClip hurtSound;
+    public float soundVolume = 1.0f;
+    private AudioSource audioSource;
+
+      void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if(audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        audioSource.spatialBlend = 1.0f;
+        audioSource.minDistance = 1f;
+        audioSource.maxDistance = 50f;
+        audioSource.rolloffMode = AudioRolloffMode.Linear;
+    }
+
     void Update()
     {
         Transform tf = transform;
@@ -13,6 +32,13 @@ public class TheGunner : MonoBehaviour
         //Shooting Input
         if (Input.GetKeyDown(shoot))
         {
+        //play sound
+            if (hurtSound != null && audioSource != null)
+            {
+                audioSource.transform.position = transform.position;
+                audioSource.PlayOneShot(hurtSound, soundVolume);
+            }
+            
             //Spawn Bullet
             Vector3 spawnPosition = transform.position + transform.up * 2f;
 
