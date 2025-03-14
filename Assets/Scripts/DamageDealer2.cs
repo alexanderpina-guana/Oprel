c2515d4e83058cfa5dfdb9a0b2b393d80eee5571
alexanderpina-guana <pinaa5533@gmail.com>
2025-03-14 10:49:53 -0700

    Added Sound Code

--- Assets/Scripts/DamageDealer2.cs
+++ Assets/Scripts/DamageDealer2.cs
@@ -4,11 +4,18 @@ public class DamageDealer2 : MonoBehaviour
 {
     public int Damage = 1;
 
+    //Sound settings
+
+    public AudioClip lazerius;
+    public float soundVolume;
+
    private void OnTriggerEnter2D(Collider2D collider2D)
    {
     if(collider2D.gameObject.TryGetComponent<Health2>(out Health2 component))
     {
         component.TakeDamage(Damage);
     }
+     
+     AudioSource.PlayClipAtPoint(lazerius, transform.position, soundVolume);
    }
 }
